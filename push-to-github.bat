0f737d791fde6e14401b96d1b00be8cd2e3d0960
Basil Okoth <basilokoth81@gmail.com.com>
2025-12-23 10:42:54 +0300

    Bot Update: 23/12/2025 10:42:54.11

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,19 +1,15 @@
 @echo off
-TITLE DigitMatch Pro - Push to GitHub
+TITLE DigitMatch Pro - Quick Update
 CD /D "%~dp0"
 
-echo [1/3] Staging changes...
+echo [1/2] Obfuscating Bot...
+node obfuscate.js
+
+echo [2/2] Pushing to GitHub...
 git add .
-
-echo [2/3] Committing changes...
-:: This uses the current date/time in the message
-git commit -m "Update obfuscated bot files - %date% %time%"
-
-echo [3/3] Pushing to GitHub...
+git commit -m "Bot Update: %date% %time%"
 git push origin main
 
 echo.
-echo ==================================================
-echo   DONE! Your files are now on GitHub.
-echo ==================================================
+echo SUCCESS! Your GitHub is updated.
 pause
